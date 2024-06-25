6e6319879c284b3de411beb9e91237d46b819dd3
sara diaz <sndiaz4@soy.sena.edu.co>
2024-06-25 13:33:12 -0500

    Esta es la ultima version 18

---
+++ fenix_web 18/src/pages/Login/credenciales.js
@@ -0,0 +1,9 @@
+// credenciales.js
+
+export const jefeCredenciales = { username: '1017062044', password: '1017062044' };
+
+export const bodeguerosCredenciales = [
+    { username: '1012318905', password: '1012318905' },
+    { username: '1032797039', password: '1032797039' },
+    // Puedes agregar más credenciales aquí
+];
